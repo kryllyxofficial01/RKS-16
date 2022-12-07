a34c8d66f4dd594d1d92371c45e9b3389aac2f89
Kryllyx <kryllyxofficial@gmail.com>
2022-12-07 16:50:48 -0500

    this took way longer to get working than im willing to admit

--- rks/src/main.rs
+++ rks/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("src/test.rks").expect("Could not find the file");
+    let reader = BufReader::new(file);
+
+    let mut lines: Vec<String> = Vec::new();
+    for line in reader.lines() {
+        lines.push(line.unwrap());
+    }
+
+    println!("{:?}", lines)
 }
