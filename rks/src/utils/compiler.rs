4ffa94cab29dbd2c91c5b5a1206c23b94160aa5a
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-12-08 19:46:55 +0000

    start on actual assembling

--- rks/src/utils/compiler.rs
+++ rks/src/utils/compiler.rs
@@ -2,6 +2,15 @@
 
 pub mod Compiler {
 	pub fn compile(line: String) {
-		println!("{line}");
+		let mut instruction: Vec<&str> = line.split(" ").collect();
+		let cmd = instruction.get(0); instruction.remove(0);
+		let args: Vec<&str> = instruction;
+
+		for arg in args {
+			let prefix = arg.chars().next().unwrap();
+			if prefix == '!' {
+				
+			}
+		}
 	}
 }
