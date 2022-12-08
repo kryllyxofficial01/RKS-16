5910aed1dcb69bd476a5bd10ff8a5a7afa006f86
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-12-08 18:35:42 +0000

    optimization

--- rks/src/main.rs
+++ rks/src/main.rs
@@ -8,12 +8,7 @@ fn main() {
     let file = File::open("src/tests/test.rks").expect("Could not find the file");
     let reader = BufReader::new(file);
 
-    let mut lines: Vec<String> = Vec::new();
     for line in reader.lines() {
-        lines.push(line.unwrap());
-    }
-
-    for line in lines {
-        Compiler::compile(line);
+        Compiler::compile(line.unwrap());
     }
 }
