34f6ce10e5182e0f6e82d0696da67d6b62da4eb2
Kryllyx <kryllyxofficial@gmail.com>
2022-12-09 17:28:31 -0500

    add the rest of the instruction names + their op_codes

--- rks/src/utils/compiler.rs
+++ rks/src/utils/compiler.rs
@@ -13,7 +13,42 @@ pub mod compiler {
 				instructions: HashMap::from(
 					[
 						("NOP", "000000"),
-						("LDA", "000001")
+						("LDA", "000001"),
+						("LDB", "000010"),
+						("LDC", "000011"),
+						("LDD", "000100"),
+						("SWP", "000101"),
+						("MOV", "000110"),
+						("STA", "000111"),
+						("STB", "001000"),
+						("STC", "001001"),
+						("STD", "001010"),
+						("STAO", "001011"),
+						("LRA", "001100"),
+						("LRB", "001101"),
+						("LRC", "001110"),
+						("LRD", "001111"),
+						("PSH", "010000"),
+						("PUL", "010001"),
+						("ISP", "010010"),
+						("DSP", "010011"),
+						("ADD", "010100"),
+						("SUB", "010101"),
+						("MUL", "010110"),
+						("DIV", "010111"),
+						("AND", "011000"),
+						("OR", "011001"),
+						("NOR", "011010"),
+						("XOR", "011011"),
+						("NOT", "011100"),
+						("INC", "011101"),
+						("DEC", "011110"),
+						("JMP", "011111"),
+						("CMP", "100000"),
+						("JZ", "100001"),
+						("JN", "100010"),
+						("JO", "100011"),
+						("HLT", "100100")
 					]
 				) 
 			}
@@ -27,7 +62,7 @@ pub mod compiler {
 			let args = &instruction[1..];
 	
 			let mut binary: Vec<String> = Vec::new();
-			binary.push(self.instructions.get(&cmd.unwrap() as &str).unwrap().to_string());
+			binary.push(self.instructions.get(&cmd.unwrap().to_uppercase() as &str).unwrap().to_string());
 	
 			for arg in args {
 				let prefix = arg.chars().next().unwrap();
