8513bfe81b5d6afa68be1b739e9e5008cc01e578
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-01-06 14:14:21 +0000

    start on finding labels

--- rks/src/main.rs
+++ rks/src/main.rs
@@ -1,5 +1,7 @@
+#![warn(non_snake_case)]
+
 use std::collections::HashMap;
-use std::fs::{self, File};
+use std::fs;
 use std::io::{BufReader, BufRead};
 use std::path::PathBuf;
 
@@ -16,39 +18,77 @@ fn main() {
 
     println!("Compiling...");
 
-    let labels: HashMap<String, u32> = find_labels(reader.lines().collect::<Result<_, _>>().unwrap());
+    let instructions: Vec<String> = clean(reader.lines().collect::<Result<_, _>>().unwrap(), location.to_str().unwrap().to_string());
+    // let labels: HashMap<String, u32> = cleaned.1;
 
-    let mut instructions: Vec<String> = Vec::new();
+    // println!("{:?}", labels);
+    
     let mut lineno = 1;
+    let mut binary: Vec<String> = Vec::new();
+    for instruction in instructions {
+        let error = Error::new(&instruction, lineno, location.to_str().unwrap().to_string());
+        let mut compiler = Compiler::new(&instruction, error);
 
-    for line in BufReader::new(file).lines() {
-        let instruction = &line.unwrap();
-        
-        if instruction != "" && &instruction[..1] != ";" {
-            let error = Error::new(instruction, lineno, location.to_str().unwrap().to_string());
-            let mut compiler = Compiler::new(instruction, error);
-
-            let mut compiled = compiler.compile();
-            if compiled.len() == 1 {
-                compiled.push("0".repeat(10).to_string());
-            }
-            instructions.push(compiled.join(""));
+        let mut compiled = compiler.compile();
+        if compiled.len() == 1 {
+            compiled.push("0".repeat(10).to_string());
         }
+        binary.push(compiled.join(""));
 
         lineno += 1;
     }
 
     println!("\nAssembling...");
-    let assembler = Assembler::new(instructions);
+    let assembler = Assembler::new(binary);
     assembler.assemble(&(location.to_str().unwrap()[0..location.to_str().unwrap().len()-4].to_string()));
 }
 
-fn find_labels(instructions: Vec<String>) -> HashMap<String, u32> {
+fn clean(mut lines: Vec<String>, location: String) -> Vec<String> {
     let mut labels: HashMap<String, u32> = HashMap::new();
+    let mut instructions: Vec<String> = Vec::new();
 
-    for instruction in instructions {
-        
+    lines.retain(|i| i.trim() != "" && !i.trim().starts_with(";"));
+
+    println!("{:?}", lines);
+    std::process::exit(0);
+
+    let mut lineno = 1;
+    let mut i: i32 = -1;
+    let mut inLabel = false;
+    for line in lines {
+        let error = Error::new(&line, lineno, location.clone());
+
+        if line.starts_with(".") {
+            let mut idx = 1;
+            let mut label = String::new();
+            while idx != line.len() {
+                label.push(line.chars().nth(idx).unwrap());
+                idx += 1;
+            }
+
+            println!("{label}");
+
+            if label == "stop" {
+                if !inLabel {
+                    error.print_stacktrace("LabelError", "Attempted to stop a label, but no label has been started".to_string())
+                }
+                else {
+                    inLabel = false;
+                }
+            }
+            else {
+                if inLabel {
+                    error.print_stacktrace("LabelError", "Attempted to start a label, but the previous label has not been closed".to_string())
+                }
+                else {
+                    inLabel = true;
+
+                }
+            }
+        }
+
+        lineno += 1;
     }
 
-    return labels;
+    return instructions;
 }
