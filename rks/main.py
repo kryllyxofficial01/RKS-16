9348bf69782b8cb1798ee9fd39355ddc9556fc40
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-01-24 14:17:43 +0000

    wip

--- rks/main.py
+++ rks/main.py
@@ -2,10 +2,8 @@ from utils.compiler import Compiler
 
 filename = "../tests/test.rks"
 file = open(filename, 'r')
+bin_file = open(filename[:-4], 'w')
 
 instructions = file.readlines()
 for instruction in instructions:
 	compiler = Compiler(instruction)
-	binary = compiler.compile()
-
-	print(binary)
