e02c8ec302d678cdada8b6a92be86d54d0d6dae1
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2023-02-23 19:05:57 +0000

    basic setup ig

--- rks/main.py
+++ rks/main.py
@@ -1,7 +1,10 @@
 import os
 
+from utils.lexer import Lexer
 from utils.error import Error
 
 filename = os.path.abspath("../tests/test.rks")
 file = open(filename, 'r')
 bin_file = open(filename[:-4], 'w')
+
+instructions = file.readlines()
---
+++ rks/utils/lexer.py
@@ -0,0 +1,6 @@
+class Lexer:
+	def __init__(self, instructions: list[str]) -> None:
+		self.instructions = instructions
+
+	def lex() -> list:
+		pass
