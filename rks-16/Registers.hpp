678dae55fa8600863f3706901c865626a10c2149
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-03-24 16:34:33 -0400

    forgot to  remove these

--- rks-16/Registers.hpp
+++ rks-16/Registers.hpp
@@ -6,13 +6,6 @@
 #include "RKS-16.hpp"
 
 struct rks::Registers {
-	// uint16_t A = 0;
-	// uint16_t B = 0;
-	// uint16_t C = 0;
-	// uint16_t D = 0;
-	// uint16_t F = 0;	
-	// uint16_t PC = 0;
-
 	uint16_t A;
 	uint16_t B;
 	uint16_t C;
