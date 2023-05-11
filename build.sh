d62cc916035d98176f57d7c6d1454c1f9db9f12b
kryllyxofficial01 <97801783+kryllyxofficial01@users.noreply.github.com>
2023-05-11 18:48:33 +0000

    change assembler sources variable

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 BUILD=build
 
-ASM_SOURCES=$(find ./assembler -type f -name "*.cpp")
+ASSEMBLER=$(find ./assembler -type f -name "*.cpp")
 
 assemble() {
 	mkdir -p $BUILD
 	mkdir -p $BUILD/rks
 
-	g++ $ASM_SOURCES -o $BUILD/rks/main
+	g++ $ASSEMBLER -o $BUILD/rks/main
 	./$BUILD/rks/main
 }
 
