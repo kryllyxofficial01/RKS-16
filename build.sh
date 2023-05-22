37d129118310606e9873619f452499c0c59a6b8e
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-05-22 08:49:14 -0400

    just because my laptop is like 3 years old doesnt mean this makes sense

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+GXX=g++
+GXX_FLAGS=-std=c++17 # Won't compile without this for some reason
+
 BUILD=build
 
 ASSEMBLER=$(find ./assembler -type f -name "*.cpp")
@@ -7,14 +10,14 @@ UTILS=$(find ./common -type f -name "*.cpp")
 assemble() {
 	mkdir -p $BUILD/assembler
 
-	g++ $UTILS $ASSEMBLER -o $BUILD/assembler/main
+	$GXX $GXX_FLAGS $UTILS $ASSEMBLER -o $BUILD/assembler/main
 	./$BUILD/assembler/main
 }
 
 emulate() {
 	mkdir -p $BUILD/emulator
 
-	g++ $UTILS $EMULATOR -o $BUILD/emulator/main
+	$GXX $GXX_FLAGS $UTILS $EMULATOR -o $BUILD/emulator/main
 	./$BUILD/emulator/main
 }
 
